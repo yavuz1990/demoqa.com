327ae0b4022f6205a8c54eee68c88c629f568556
yavuz1990 <115427573+yavuz1990@users.noreply.github.com>
2023-10-02 03:47:35 +0300

    commitsonnnn

--- src/test/java/com/demoqa/step_definitions/login_StepDefinitions.java
+++ src/test/java/com/demoqa/step_definitions/login_StepDefinitions.java
@@ -19,6 +19,7 @@ import static com.demoqa.utilities.Driver.getDriver;
 public class login_StepDefinitions {
     LoginPage login=new LoginPage();
     WebDriverWait wait=new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(15));
+
     //WebElement element = driver.findElement(By.id("id_of_element"));
     //WebElement element = getDriver().findElement(By.xpath("//div[@class='element-list collapse show']//li[@id='item-0']"));
     //Faker faker=new Faker();
@@ -43,7 +44,9 @@ public class login_StepDefinitions {
         login.loginSubmodule.click();
     }
     @And("user enters correct Username")
-    public void user_enters_correct_username() {
+    public void user_enters_correct_username() throws InterruptedException {
+        Thread.sleep(5);
+        wait.until(ExpectedConditions.visibilityOf(login.inputUsername));
         String userName = ConfigurationReader.getProperty("username");
         login.inputUsername.sendKeys(userName);
     }
@@ -82,6 +85,6 @@ public class login_StepDefinitions {
         String actualMessage = login.actualMessage.getText();
         String expectedMessage = "Invalid username or password!";
         Assert.assertEquals(expectedMessage,actualMessage);
-
     }
+
 }
